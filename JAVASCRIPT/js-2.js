62d8e18f6e919029ca6330aed7e8dfd718a8f45b
Bharat Kumar <100489720+bharatkumarmali@users.noreply.github.com>
2023-10-04 12:53:21 +0530

    Add files via upload

---
+++ JAVASCRIPT/js-2.js
@@ -0,0 +1,12 @@
+a = 2;
+b = 3;
+c = a + b;
+
+// document.write("the value of c is = " + c)
+
+// window.alert("the value of c is =  " + c)
+// alert("the value of c is =  " + c)
+
+document.getElementById("id").innerHTML = + "the value of c is = " + C;
+
+// console.log(c);  
